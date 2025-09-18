a79b3d15642167094e3bc64e4a17b43df2f15db5
carlaum167 <carlosgado167@gmail.com>
2025-09-18 13:55:52 -0300

    :sparkles: feat: adicionado a dependencias do axios e criada função com useRef

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,18 +1,26 @@
 import { Stack } from 'expo-router';
 import '../../../global.css';
 import { View,TouchableOpacity, Text, Button, TextInput,} from 'react-native';
+import { useRef} from 'react';
 import { router } from 'expo-router';
 
 import { ScreenContent } from '../../components/ScreenContent';
 
 export default function Home() {
+  const inputRef = useRef<TextInput>(null);
+
+  function CitySearch() {
+
+  }
+
   return (
+
     <>
       <Stack.Screen options={{ title: 'Tab One' }} />
       <View>
         <TouchableOpacity onPress={() => router.push('/cadastro')}>
           <Text className="text-lg font-bold text-blue-600">Previsao do tempo</Text>
-          <TextInput placeholder="Digite sua cidade" />
+          <TextInput ref={inputRef} placeholder="Digite sua cidade" />
           <Button title="Buscar" onPress={() => {}} />
         <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
         </TouchableOpacity>
