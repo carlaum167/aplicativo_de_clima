663cf5c038f3e72d6d12cdde457464a3fa3c8dd4
carlaum167 <carlosgado167@gmail.com>
2025-09-18 14:20:03 -0300

    :sparkles: feat: conexão com api, e adição de funcionalidades aos componentes

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,22 @@
+import React, { useRef } from 'react';
 import { Stack } from 'expo-router';
 import '../../../global.css';
 import { View,TouchableOpacity, Text, Button, TextInput,} from 'react-native';
-import { useRef} from 'react';
 import { router } from 'expo-router';
 
 import { ScreenContent } from '../../components/ScreenContent';
+import axios from 'axios';
 
 export default function Home() {
   const inputRef = useRef<TextInput>(null);
+  const [city, setCity] = React.useState('');
 
-  function CitySearch() {
+  async function CitySearch() {
+    const keyApi = 'f0389190a410566fa6c742f7488cd7c1';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyApi}`;
+    const data = await axios.get(url);
 
+    console.log(data);
   }
 
   return (
@@ -20,8 +26,13 @@ export default function Home() {
       <View>
         <TouchableOpacity onPress={() => router.push('/cadastro')}>
           <Text className="text-lg font-bold text-blue-600">Previsao do tempo</Text>
-          <TextInput ref={inputRef} placeholder="Digite sua cidade" />
-          <Button title="Buscar" onPress={() => {}} />
+          <TextInput
+            ref={inputRef}
+            placeholder="Digite sua cidade"
+            value={city}
+            onChangeText={setCity}
+          />
+          <Button title="Buscar" onPress={CitySearch} />
         <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
         </TouchableOpacity>
       </View>
